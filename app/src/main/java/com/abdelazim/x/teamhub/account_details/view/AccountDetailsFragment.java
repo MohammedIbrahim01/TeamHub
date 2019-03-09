6432a29e9f57e341a55ba65fb7ca084b7eaca33f
Ahmed Khairy <Ahmedkhairyitpro@gmail.com>
2019-03-09 21:20:54 +0200

    Add avatar ImageView

--- app/src/main/java/com/abdelazim/x/teamhub/account_details/view/AccountDetailsFragment.java
+++ app/src/main/java/com/abdelazim/x/teamhub/account_details/view/AccountDetailsFragment.java
@@ -1,13 +1,19 @@
 package com.abdelazim.x.teamhub.account_details.view;
 
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.abdelazim.x.teamhub.R;
@@ -15,6 +21,8 @@ import com.abdelazim.x.teamhub.account_details.AccountContract;
 import com.abdelazim.x.teamhub.account_details.presenter.AccountPresenter;
 import com.abdelazim.x.teamhub.home.view.HomeFragment;
 import com.abdelazim.x.teamhub.repository.Account;
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.Picasso;
 
 
 /**
@@ -24,7 +32,7 @@ public class AccountDetailsFragment extends Fragment implements AccountContract.
 
     private AccountPresenter presenter;
     private TextView account_data_textView;
-
+    private ImageView avatarImageView;
 
 
 
@@ -50,6 +58,7 @@ public class AccountDetailsFragment extends Fragment implements AccountContract.
     private void initView(View view) {
         account_data_textView =view.findViewById(R.id.account_data_textView);
         String name = getArguments().getString(HomeFragment.KEY_ACCOUNT_NAME);
+        avatarImageView = view.findViewById(R.id.avatar_imageView);
         data_display(name);
     }
 
@@ -74,6 +83,24 @@ public class AccountDetailsFragment extends Fragment implements AccountContract.
         account_data_textView.append("User Updated at : "+account.getUpdated_at()+"\n"+"\n");
         account_data_textView.append("URL : "+ "\n"+account.getUrl()+"\n"+"\n");
         account_data_textView.append("HTML URL : "+"\n"+account.getHtml_url());
+
+
+        Picasso.get().load(account.getAvatar_url()).into(avatarImageView, new Callback() {
+            @Override
+            public void onSuccess() {
+                Bitmap imageBitmap = ((BitmapDrawable)avatarImageView.getDrawable()).getBitmap();
+                RoundedBitmapDrawable imageRoundedDrawable = RoundedBitmapDrawableFactory.create(getResources(),imageBitmap);
+                imageRoundedDrawable.setCircular(true);
+                imageRoundedDrawable.setCornerRadius(Math.max(imageBitmap.getWidth(),imageBitmap.getHeight())/2.0f);
+                avatarImageView.setImageDrawable(imageRoundedDrawable);
+            }
+
+            @Override
+            public void onError(Exception e) {
+
+            }
+        });
+
 
     }
 }
